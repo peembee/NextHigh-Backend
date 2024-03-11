c52af10473b3164ce528aedbd22338af5498ea47
peembee <p.boukhedimi@hotmail.com>
2024-03-11 11:50:04 +0100

    password is not in getAPI-Persons anymore

--- GoApptechBackend/Models/DTO/PersonDTO/GetPersonDTO.cs
+++ GoApptechBackend/Models/DTO/PersonDTO/GetPersonDTO.cs
@@ -15,7 +15,6 @@ namespace GoApptechBackend.Models.DTO.PersonDTO
         public string Username { get; set; } = string.Empty;
 
         public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
 
         public string LastName { get; set; } = string.Empty;
